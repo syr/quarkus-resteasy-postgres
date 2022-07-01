95037da73bbe51ed65ed8bfea961538001972a67
CS <syrious3000@yahoo.de>
2022-07-01 22:16:59 +0200

    read job typ from service annotation

--- src/main/java/org/acme/TestResource.java
+++ src/main/java/org/acme/TestResource.java
@@ -1,16 +1,18 @@
 package org.acme;
 
 import io.quarkus.arc.All;
+import io.quarkus.arc.InstanceHandle;
 import io.quarkus.runtime.StartupEvent;
+import org.acme.model.JobType;
 import org.acme.service.Service;
 
 import javax.enterprise.event.Observes;
-import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 import java.util.logging.Logger;
 
 @Path("/test")
@@ -19,11 +21,13 @@ public class TestResource {
     private static final Logger LOG = Logger.getLogger("DownloadSyncResource");
 
     @Inject @All
-    Instance<Service> service;
+    List<InstanceHandle<Service>> services;
 
     void onStart(@Observes StartupEvent ev) {
-        Service service = this.service.get();
+        JobType jobTypeFromAnnotation = ((SupportsJobType) services.get(0).getBean().getQualifiers().stream().filter(q -> q.annotationType() == SupportsJobType.class).findFirst().get()).value();
+        //TODO in service method: determine which service to use by poassed JobEntity
         LOG.info("test: ");
+//        Arc.container().listAll(Service.class, SupportsJobType.class);
     }
 
     @GET
